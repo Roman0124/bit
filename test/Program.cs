3ac45c30e6abdc54f42417cc0076afa26ee4c42e
Roman <roma.loboda.97@bk.ru>
2022-10-21 21:30:28 +0700

    создали рипозиторий

---
+++ test/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Привет,мир"); 
+Console.Write("Введите имя ");
+string name = Console.ReadLine();
+Console.WriteLine(name);
+Console.Write("Введите возраст ");
+int age = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine (age);
+Console.WriteLine(age + 10);
