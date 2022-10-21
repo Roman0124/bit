4274de85ee08542c2d036ba8a1c921921fe3b867
Roman <roma.loboda.97@bk.ru>
2022-10-21 22:36:14 +0700

    добавили 4 задачу

--- задача 4/Program.cs
+++ задача 4/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+Console.Write("Введите а");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите c");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a>b & a>c)
+Console.Write ("a max");
+if (b>a & b>c)
+Console.Write ("b max");
+if (c>a & c>b)
+Console.Write ("c max");
--- задача 4/obj/Debug/net6.0/задача 4.AssemblyInfo.cs
+++ задача 4/obj/Debug/net6.0/задача 4.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("задача 3")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("задача 4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("задача 3")]
-[assembly: System.Reflection.AssemblyTitleAttribute("задача 3")]
+[assembly: System.Reflection.AssemblyProductAttribute("задача 4")]
+[assembly: System.Reflection.AssemblyTitleAttribute("задача 4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
