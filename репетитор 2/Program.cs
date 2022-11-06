818f5bc9fabc5dbc958bd86c6d6051eaa0571d1b
Roman <roma.loboda.97@bk.ru>
2022-11-06 21:48:25 +0700

    10 задача

---
+++ репетитор 2/Program.cs
@@ -0,0 +1,18 @@
+﻿//  Вводим с клавиатуры размер 3 сторон треугольника.
+//   И говорим какой он - обычный, равнобедренный или равносторонний
+//   a = 6
+//   b = 8
+//   c = 6
+Console.Write("Введите а");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите c");
+int c = Convert.ToInt32(Console.ReadLine());
+if (a + b > c)
+Console.Write ("обычный");
+if (a + c > b)
+Console.Write ("равнобедренный");
+if (b + c > a)
+Console.Write ("равносторонний");
+
