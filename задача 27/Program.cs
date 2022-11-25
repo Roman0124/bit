48ca364113e33763c67279c2f2145f14baf57879
Roman <roma.loboda.97@bk.ru>
2022-11-25 21:16:43 +0700

    задача 27

---
+++ задача 27/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+// 452 -> 11
+
+// 82 -> 10
+
+// 9012 -> 12
+Console.WriteLine("input a");
+int a = Convert.ToInt32(Console.ReadLine());
+int Rezalt = 0;
+while (a > 0)
+{
+    Rezalt += a % 10;
+    a = a / 10;
+}
+Console.WriteLine (Rezalt);
